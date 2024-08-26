1a632243ad62790e9ef042ee2af3856b383462bb
Li-Yi Wei <lwei@adobe.com>
2024-08-26 12:00:49 -0700

    update starting up stuff

--- start.sh
+++ start.sh
@@ -2,6 +2,9 @@
 
 sudo apt install -y tmux
 sudo apt install ffmpeg
+conda create -n comfyui python=3.11 -c conda-forge
+conda activate comfyui
+pip install -r requirements.txt
 session_name=comfyUI
 tmux kill-session -t ${session_name}
 conda init bash
