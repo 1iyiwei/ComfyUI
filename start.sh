4f99e51e54d171afa4d6bdd3002d39587eb9e17c
Li-Yi Wei <lwei@adobe.com>
2024-12-29 23:16:09 -0800

    remove redundant parts of pip install and add pip install for custom nodes

--- start.sh
+++ start.sh
@@ -15,11 +15,15 @@ conda create -n comfyui python=3.11 -c conda-forge
 conda activate comfyui
 pip install --upgrade pip
 pip install -r requirements.txt
+for custom_node in custom_nodes/*; do
+    custom_node_requirement=${custom_node}/requirements.txt
+    if [ -f ${custom_node_requirement} ]; then
+        echo ${custom_node_requirement}
+        pip install -r ${custom_node_requirement}
+    fi
+done
 session_name=comfyUI
-tmux kill-session -t ${session_name}
-conda create -n comfyui python=3.11 -c conda-forge
-conda activate comfyui
-pip install -r requirements.txt
+#tmux kill-session -t ${session_name}
 tmux new-session -d -s ${session_name} "python main.py --listen --port 8188 --prefix ${prefix} 2>&1 | tee ${session_name}.log"
 
 if [ -n "$sleep_amount" ]; then
